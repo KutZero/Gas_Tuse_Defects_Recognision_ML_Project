aee925a6c37e84954348c2a9f90e2239224d342b
kutzero <eugenecalls@gmail.com>
2024-05-02 00:45:50 +0700

    feat(model test): add test res for AE model_id=v01n01

--- common_dependencies.py
+++ common_dependencies.py
@@ -1,43 +1,102 @@
 import os
 import re
 import sys
-import pandas as pd
-import numpy as np
 import logging
 import itertools
+import numpy as np
+import pandas as pd
 
-from IPython.display import display
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
 import matplotlib.pyplot as plt
 from matplotlib import ticker
 from matplotlib.patches import Rectangle
 
-import keras_cv
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.layers import (Reshape, Input, Dense, Flatten, Conv2D, Dropout, 
-    Conv2DTranspose, Lambda, MaxPooling2D, UpSampling2D, concatenate, BatchNormalization, Normalization)
+from IPython.display import display
+from typing import NamedTuple
 
-#import custom_modules.pipe_data as pidf
 import custom_modules.data_worker as dw
 
 os.environ["ROCM_PATH"] = "/opt/rocm"
 os.environ["MLIR_CRASH_REPRODUCER_DIRECTORY"] = "enable"
-os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
+#os.environ["TF_GPU_ALLOCATOR"] = "cuda_malloc_async"
 
 # create logger
 logger = logging.getLogger('main')
-logger.setLevel(logging.ERROR)
-
+logger.setLevel(logging.DEBUG)
 # create console handler and set level to debug
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-
 # create formatter
 formatter = logging.Formatter('%(name)s :: %(funcName)20s() :: %(message)s')
-
 # add formatter to ch
 ch.setFormatter(formatter)
-
 # add ch to logger
 logger.addHandler(ch)
+
+PATH_TO_DATA = {
+    'run_1': 
+        ('data/original_data/run_1/run1_WM32_data.csv',
+        'data/original_data/run_1/run1_WM32_defects.csv',
+        'data/original_data/run_1/run1_WM32_pipe.csv'),
+    'run_2':
+        ('data/original_data/run_2/run2_WM32_data.csv',
+        'data/original_data/run_2/run2_WM32_defects.csv',
+        'data/original_data/run_2/run2_WM32_pipe.csv')
+}
+
+DataCrop = NamedTuple("DataCrop", [('left', int), ('top', int), ('width', int), ('height', int)])
+SlidingCrop = NamedTuple("SlidingCrop", [('crop_size', int), ('crop_step', int)])
+DatasetPartDescription = NamedTuple("DatasetPartDescription", 
+                                    [('data_path_tuple', tuple[str,str,str]), 
+                                     ('file_data_crop', DataCrop), 
+                                     ('sliding_crop', SlidingCrop),
+                                     ('data_x_shift', int)])
+
+def get_dataset_gen(desc_part: DatasetPartDescription):
+    logger.debug("start")
+
+    left = desc_part.file_data_crop.left
+    top = desc_part.file_data_crop.top
+    width = desc_part.file_data_crop.width
+    height = desc_part.file_data_crop.height
+    crop_size = desc_part.sliding_crop.crop_size
+    crop_step = desc_part.sliding_crop.crop_step
+    
+    x_df, y_df = dw.get_x_and_y_data(*desc_part.data_path_tuple)
+    x_df = dw.roll_df(x_df, desc_part.data_x_shift, 1)
+    y_df = dw.roll_df(y_df, desc_part.data_x_shift, 1)
+
+    x_arr = dw.df_to_numpy(x_df)
+    y_arr = y_df.to_numpy()
+
+    x_arr = np.concatenate([dw.normalize_data(x_arr[:,:,:32]), dw.normalize_data(x_arr[:,:,32:])],axis=2)
+    y_arr = dw.normalize_data(y_arr)
+    
+    x_arr = x_arr[top:top+height, left:left+width]
+    y_arr = y_arr[top:top+height, left:left+width]
+    
+    x_df = dw.extend_ndarray_for_crops_dividing(x_arr, crop_size, crop_step)
+    y_df = dw.extend_ndarray_for_crops_dividing(y_arr, crop_size, crop_step)
+    
+    #x_time_crops_gen = dw.get_crop_generator(x_arr_time, crop_size, crop_step)
+    #x_amp_crops_gen = dw.get_crop_generator(x_arr_amp, crop_size, crop_step)
+    x_crops_gen = dw.get_crop_generator(x_arr, crop_size, crop_step)
+    y_crops_gen = dw.get_crop_generator(y_arr, crop_size, crop_step)
+    
+    y_binary_gen = (1 if np.sum(crop > 0) else 0 for crop in dw.get_crop_generator(y_arr, crop_size, crop_step))
+    #y_depth_gen = (np.max(crop) for crop in dw.get_crop_generator(y_arr, crop_size, crop_step))
+
+    logger.debug("end")
+    #return x_time_crops_gen, x_amp_crops_gen, y_bool_crops_gen, y_binary_gen, y_depth_gen
+    return (x_crops_gen, y_crops_gen, y_binary_gen)
+
+def chain_dataset_gens(datasets_desc: tuple[DatasetPartDescription]):
+    # get output data tuple for each data part from dataset_desc
+    data_tuples_arr = [get_dataset_gen(desc_part) for desc_part in datasets_desc]
+    # concat data tuples throught 1 axis (like concat every first item from all tuple into one first item of result tuple, same to second and etc.)
+    # output tuple has the same quantity of values as every tuple got from previous step but each element of it is concatination of all elements
+    # from that position from all read tuples
+    return (*[itertools.chain(*[arr for arr in item]) for item in zip(*data_tuples_arr)],)
