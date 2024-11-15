51b406b3cd726c6a42f0e16b30d11cd448a4b8df
KutZero <eugenecalls@gmail.com>
2024-11-15 19:42:50 +0700

    feat(logging): add loger.py file to create main logger when imported

---
+++ loger.py
@@ -0,0 +1,13 @@
+# create logger
+
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter
+formatter = logging.Formatter('%(name)s :: %(funcName)20s() :: %(message)s')
+# add formatter to ch
+ch.setFormatter(formatter)
+# add ch to logger
+logger.addHandler(ch)
